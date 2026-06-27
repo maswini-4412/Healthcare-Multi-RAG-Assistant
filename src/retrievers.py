17a28ba43170646ade50a05eecbbc0a9b61fef45
maswini-4412 <maswini9267@gmail.com>
2026-06-27 11:32:35 +0530

    Fix FAISS path for Render

--- src/retrievers.py
+++ src/retrievers.py
@@ -1,60 +1,45 @@
+from pathlib import Path
 from langchain_community.vectorstores import FAISS
 
 from src.embeddings import get_embeddings
+
+# Project root directory
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+FAISS_DIR = BASE_DIR / "faiss_indexes"
 
 
 def load_retrievers():
 
     embeddings = get_embeddings()
 
-
     medical_db = FAISS.load_local(
-        "faiss_indexes/medical_index",
+        str(FAISS_DIR / "medical_index"),
         embeddings,
         allow_dangerous_deserialization=True
     )
-
 
     drug_db = FAISS.load_local(
-        "faiss_indexes/drug_index",
+        str(FAISS_DIR / "drug_index"),
         embeddings,
         allow_dangerous_deserialization=True
     )
-
 
     symptom_db = FAISS.load_local(
-        "faiss_indexes/symptom_index",
+        str(FAISS_DIR / "symptom_index"),
         embeddings,
         allow_dangerous_deserialization=True
     )
-
 
     who_db = FAISS.load_local(
-        "faiss_indexes/who_index",
+        str(FAISS_DIR / "who_index"),
         embeddings,
         allow_dangerous_deserialization=True
     )
 
-
     return {
-
-        "medical":
-            medical_db.as_retriever(
-                search_kwargs={"k":3}
-            ),
-
-        "drug":
-            drug_db.as_retriever(
-                search_kwargs={"k":3}
-            ),
-
-        "symptom":
-            symptom_db.as_retriever(
-                search_kwargs={"k":3}
-            ),
-
-        "who":
-            who_db.as_retriever(
-                search_kwargs={"k":3}
-            )
+        "medical": medical_db.as_retriever(search_kwargs={"k": 3}),
+        "drug": drug_db.as_retriever(search_kwargs={"k": 3}),
+        "symptom": symptom_db.as_retriever(search_kwargs={"k": 3}),
+        "who": who_db.as_retriever(search_kwargs={"k": 3}),
     } 
